b53fa4655bb038688f0497d963502e6cc5ac7936
christianonlay <onlaychristian99@gmail.com>
2025-09-04 08:55:30 +0800

    added interface ang parameter for menu choices and conditional statement for the selected choices

--- factorial.js
+++ factorial.js
@@ -10,3 +10,17 @@ console.log("Factorial Appication");
 console.log("1. say hello");
 console.log("2. Compute Factorial");
 console.log("3. exit");
+
+rl.question("Enter your choice (1-3)", choice =>{
+    if(choice==="1"){
+        console.log ("hello there!");
+    } else if (choice==="2"){
+        console.log ("factorial!");
+    } else if (choice==="3"){
+         console.log ("exit!");
+    } else{
+        console.log("Invalid Choice.Please Try Again");
+    }
+
+
+})
