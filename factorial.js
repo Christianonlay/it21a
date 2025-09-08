937d55b7234b58a56034d7a345539b78edb0c1cd
Christianonlay <onlaychristian99@g,gmail.com>
2025-09-08 09:19:42 +0800

    added factorial

--- factorial.js
+++ factorial.js
@@ -18,7 +18,7 @@ rl.question("enter your choice (1-3)", choice =>{
         console.log ("factorial!");
 
         rl.question("Enter a number for factorial: ", (numStr)=>{
-            let num = parseInt(numStr);}
+            let num = parseInt(numStr);
               
             if (isNaN(num) ) || num <0){
                 console.log("Invalid number.please try agian");
@@ -36,16 +36,6 @@ rl.question("enter your choice (1-3)", choice =>{
             console.log(Invalid choice. please try agian.");
 
         }
-} else if (choice=="3"){
-console.log (Existing Program... Goodbye!");
+    } else
+       
 
-} else {
-      
-});
-function backtomenu(){
-    console.log("--------");
-    console.log("1. back to menu");
-    console.log("2. exist");
-    rl.question(what would you like to do next? (1-2)) :", (backtomenuchoice)
-}
- 
