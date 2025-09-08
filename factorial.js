0db7e5ceeff897fce3a958daff08356066bc1a6b
Christianonlay <onlaychristian99@g,gmail.com>
2025-09-08 10:07:53 +0800

    added logic

--- factorial.js
+++ factorial.js
@@ -4,6 +4,8 @@ const rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 })
+function mainMenu(){
+
 
 console.log("factorial appication");
 
@@ -11,31 +13,22 @@ console.log("1. say hello");
 console.log("2. compute factorial");
 console.log("3. exit");
 
-rl.question("enter your choice (1-3)", choice =>{
+
+
+rl.question("Enter your choice (1-3):", choice =>{
     if(choice==="1"){
         console.log ("hello there!");
     } else if (choice==="2"){
-        console.log ("factorial!");
+         computerfactorial();
+    } else if (choice==="3"){
+        console.log("Existing program...Goodbye");
+        rl.close();
+    } else{
+        console.log("Invalid choice. Please Try agian");
+        mainMenu();
 
-        rl.question("Enter a number for factorial: ", (numStr)=>{
-            let num = parseInt(numStr);
-              
-            if (isNaN(num) ) || num <0){
-                console.log("Invalid number.please try agian");
-            } else{
-    
+    }
+})
 
-         let fact = 1;
-         for(let i = 1; i <= num ; i i++){
-            fact *=i;
 
-         }
-        console.log ("The factorial of "+ num + " fact);
-        
-        } else{
-            console.log(Invalid choice. please try agian.");
-
-        }
-    } else
-       
-
+}
