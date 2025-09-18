5ef4efdbbed93029e7511a250777a3b7e801980b
Christianonlay <onlaychristian99@gmail.com>
2025-09-18 08:51:39 +0800

    added partial hero

--- hero.js
+++ hero.js
@@ -5,18 +5,38 @@ class Hero{
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.item = [];
 
     }
     getHealth(){
-        return this.#health;
+        return this.#health;  
     }
+    getAttack(){
+        console.log(this.attack);
+    }
+    getName(){
+        console.log(this.name);
+    }
+    getStats(){
+        console.log("/n");
+    }
+        
     getStats(){
         console.log("Name: " + this.name);
         console.log("Health: " + this.#health);
         console.log("Attack: " + this.attack);   
     }
+
+addItem(item){
+    this.Item.push(item);
 }
+totalAttack(){
+    return this.attack +this.DataTransferItemList.reduce((sum,i)=>sum + i.bonusAttack,0);
+}
+}
+
 class Warrior extends Hero{
+
     useAbility(){
         console.log(`${this.name} uses power Strike`);
     }
@@ -32,10 +52,35 @@ useAbility(){
     console.log(`${this.name} casts Fireball`);
     }
 }
-performAbality(Thorin);
-performAbality(Gandalf);
+class Item{
+    constructor(name,bunosAttack){
+        this.name = name;
+        this.bunosAttack = bunosAttack;
+    }
+}
+
+function performAbality(Hero){
+    console.log("\n");
+    Hero.useAbility();
+}
+const sword =  new Item("Sword, 5");
+const staf = new Item("staff", 3);
 
 const warrior = new Warrior("Thorin",100,10);
+
+//Thorin.useAbility();
+//Thorin.getStats();
+//Thorin.getAttack();
+Thorin.getName();
+thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+onsole.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+onsole.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+onsole.log(Thorin.totalAttack());
+
 
 //Thorin.useAbility();
 Thorin.getStats();
@@ -44,10 +89,6 @@ const mage = new Mage("Gandalf",80,8,50);
 //Gandalf.useAbility();
 Gandalf.getStats();
 
-function performAbality(Hero){
-    console.log("\n");
-    Hero.useAbility();
-}
 performAbality(Thorin);
 performAbality(Gandalf);
 
