570b579ef97085c5ed710d8c714028f7aefecdd1
jeboyyyy <chanchanonlay@gmail.com>
2025-10-16 08:01:44 +0800

    Added basic linechart

---
+++ person.js
@@ -0,0 +1,25 @@
+class person{
+     
+    constructor(name,age,occupition){
+        this.nam
+        e = name;
+        this.age = age;
+        this.occupition = occupition;
+
+    }
+     getName(){
+        console.log(this.name);
+    }
+    displayInfo(){
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: + this.occupation");
+        console.log("----------")
+
+    }  
+}
+
+const person1 = new person("Coco Martin",24,"Masahista");
+
+person1.getName();
+person1.displayInfo();
